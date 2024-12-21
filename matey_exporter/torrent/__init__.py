d3dedb7469f650c3fc259c3d6abf3997a17a51c9
Dennis <scienta@live.se>
2024-12-21 22:46:19 +0100

    Added initial support for Deluge

--- matey_exporter/torrent/__init__.py
+++ matey_exporter/torrent/__init__.py
@@ -8,7 +8,12 @@ def load_qbittorrent(**kwargs):
     from .qbittorrent import MateyQbittorrent
     return MateyQbittorrent(**kwargs)
 
+def load_deluge(**kwargs):
+    from .deluge import MateyDeluge
+    return MateyDeluge(**kwargs)
+
 torrent_loader = {
     'Transmission': load_transmission,
     'Qbittorrent': load_qbittorrent,
+    'Deluge': load_deluge,
 }
