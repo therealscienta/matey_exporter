33424b28fa9f2e2062ae4900424f76de65e2d0e2
Dennis <scienta@live.se>
2025-03-09 09:52:20 +0100

    Updated names in tests and added tests to be implemented

--- tests/test_deluge.py
+++ tests/test_deluge.py
@@ -1,6 +1,7 @@
 import pytest
-from matey_exporter.torrent import load_deluge
-from matey_exporter.torrent.deluge import MateyDeluge, MateyDelugePrometheusMetrics
+from matey_exporter.torrent import load_deluge_full, load_deluge_simple
+from matey_exporter.torrent.deluge_simple import MateyDelugeSimple, MateyDelugePrometheusMetricsSimple
+from matey_exporter.torrent.deluge_full import MateyDelugeFull, MateyDelugePrometheusMetricsFull
 
 good_test_config_1 = {
     "deluge": [
@@ -19,15 +20,28 @@ good_test_config_2 = {
     ],
 }
 
-def test_good_config_deluge():
-    deluge = load_deluge(**good_test_config_1['deluge'][0])
-    assert isinstance(deluge, MateyDeluge)
-    assert deluge.metrics is MateyDelugePrometheusMetrics()
+def test_good_config_deluge_simple():
+    deluge = load_deluge_simple(**good_test_config_1['deluge'][0])
+    assert isinstance(deluge, MateyDelugeSimple)
+    assert deluge.metrics is MateyDelugePrometheusMetricsSimple()
     assert deluge.instance_name == "deluge-one"
     assert deluge.host_url == "http://192.168.1.100:8989"
     assert deluge.password == "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
     
-def test_deluge_metrics_singleton():
-    deluge_1 = load_deluge(**good_test_config_1['deluge'][0])
-    deluge_2 = load_deluge(**good_test_config_2['deluge'][0])
+def test_deluge_metrics_singleton_simple():
+    deluge_1 = load_deluge_simple(**good_test_config_1['deluge'][0])
+    deluge_2 = load_deluge_simple(**good_test_config_2['deluge'][0])
     assert deluge_1.metrics is deluge_2.metrics
+    
+# def test_good_config_deluge_full():
+#     deluge = load_deluge_full(**good_test_config_1['deluge'][0])
+#     assert isinstance(deluge, MateyDelugeFull)
+#     assert deluge.metrics is MateyDelugePrometheusMetricsFull()
+#     assert deluge.instance_name == "deluge-one"
+#     assert deluge.host_url == "http://192.168.1.100:8989"
+#     assert deluge.password == "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+    
+# def test_deluge_metrics_singleton_full():
+#     deluge_1 = load_deluge_full(**good_test_config_1['deluge'][0])
+#     deluge_2 = load_deluge_full(**good_test_config_2['deluge'][0])
+#     assert deluge_1.metrics is deluge_2.metrics
--- tests/test_qbittorrent.py
+++ tests/test_qbittorrent.py
@@ -1,6 +1,7 @@
 import pytest
-from matey_exporter.torrent import load_qbittorrent
-from matey_exporter.torrent.qbittorrent import MateyQbittorrent, MateyQbittorrentPrometheusMetrics
+from matey_exporter.torrent import load_qbittorrent_full, load_qbittorrent_simple
+from matey_exporter.torrent.qbittorrent_simple import MateyQbittorrentSimple, MateyQbittorrentPrometheusMetricsSimple
+from matey_exporter.torrent.qbittorrent_full import MateyQbittorrentFull, MateyQbittorrentPrometheusMetricsFull
 
 good_test_config_1 = {
     "qbittorrent": [
@@ -19,15 +20,28 @@ good_test_config_2 = {
     ],
 }
 
-def test_good_config_qbittorrent():
-    qbittorrent = load_qbittorrent(**good_test_config_1['qbittorrent'][0])
-    assert isinstance(qbittorrent, MateyQbittorrent)
-    assert qbittorrent.metrics is MateyQbittorrentPrometheusMetrics()
+def test_good_config_qbittorrent_simple():
+    qbittorrent = load_qbittorrent_simple(**good_test_config_1['qbittorrent'][0])
+    assert isinstance(qbittorrent, MateyQbittorrentSimple)
+    assert qbittorrent.metrics is MateyQbittorrentPrometheusMetricsSimple()
     assert qbittorrent.instance_name == "qbittorrent-one"
     assert qbittorrent.host_url == "http://192.168.1.100:8989"
     assert qbittorrent.api_key == "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
     
-def test_qbittorrent_metrics_singleton():
-    qbittorrent_1 = load_qbittorrent(**good_test_config_1['qbittorrent'][0])
-    qbittorrent_2 = load_qbittorrent(**good_test_config_2['qbittorrent'][0])
+def test_qbittorrent_metrics_singleton_simple():
+    qbittorrent_1 = load_qbittorrent_simple(**good_test_config_1['qbittorrent'][0])
+    qbittorrent_2 = load_qbittorrent_simple(**good_test_config_2['qbittorrent'][0])
     assert qbittorrent_1.metrics is qbittorrent_2.metrics
+    
+# def test_good_config_qbittorrent_full():
+#     qbittorrent = load_qbittorrent_full(**good_test_config_1['qbittorrent'][0])
+#     assert isinstance(qbittorrent, MateyQbittorrentFull)
+#     assert qbittorrent.metrics is MateyQbittorrentPrometheusMetricsFull()
+#     assert qbittorrent.instance_name == "qbittorrent-one"
+#     assert qbittorrent.host_url == "http://192.168.1.100:8989"
+#     assert qbittorrent.api_key == "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+    
+# def test_qbittorrent_metrics_singleton_full():
+#     qbittorrent_1 = load_qbittorrent_full(**good_test_config_1['qbittorrent'][0])
+#     qbittorrent_2 = load_qbittorrent_full(**good_test_config_2['qbittorrent'][0])
+#     assert qbittorrent_1.metrics is qbittorrent_2.metrics
